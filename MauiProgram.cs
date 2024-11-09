12e38dcc94dd7a09cbedfd1fa0e35497731a6e6d
Doni011 <adoniasguilherme10@hotmail.com>
2024-11-09 03:11:50 -0300

    Novos

---
+++ MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiAppHotel1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Eater-Regular.ttf", "Eater");
+                    fonts.AddFont("MysteryQuest-Regular.ttf", "MysteryQuest");
+                    fonts.AddFont("RoadRage-Regular.ttf", "RoadRage");
+                    fonts.AddFont("RubikBeastly-Regular.ttf", "RubikBeastly");
+                    fonts.AddFont("RubikWetPaint-Regular.ttf", "RubikWetPaint");
+                    fonts.AddFont("Creepster-Regular.ttf", "Creepster");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
